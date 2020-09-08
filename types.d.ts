4c2416e0fd234bc50fc1e18f2d9bc59c27e29050
UpperCod <mtrujillo@outlook.cl>
2020-09-08 20:37:16 -0300

    update types

--- types.d.ts
+++ types.d.ts
@@ -2,17 +2,5 @@ import { Context } from "@uppercod/imported";
 import { Transformer } from "postcss";
 
 declare module "@uppercod/postcss-import" {
-    export interface Options {
-        tree?: Context;
-        readFile?: (file: string) => Promise<string>;
-    }
-    export interface parallel {
-        [file: string]: {
-            [params: string]: boolean;
-        };
-    }
-    export default function pluginImport(
-        options?: Options,
-        parallel?: parallel
-    ): Transformer;
+    export default function pluginImport(tree: Context): Transformer;
 }
