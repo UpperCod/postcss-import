fb2c4c44218171efae2e68fed1bd488a1f03d8ea
UpperCod <mtrujillo@outlook.cl>
2020-09-25 00:52:09 -0300

    update .d.ts

--- types.d.ts
+++ types.d.ts
@@ -1,4 +1,4 @@
-import { Transformer, ChildNode } from "postcss";
+import { Plugin, ChildNode } from "postcss";
 
 declare module "@uppercod/postcss-import" {
     export type Nodes = ChildNode[];
@@ -24,5 +24,30 @@ declare module "@uppercod/postcss-import" {
         imports?: Imports;
         process?: Process;
     }
-    export default function pluginImport(options?: Options): Transformer;
+
+    export interface ResolveResult {
+        /**
+         * module path
+         */
+        id: string;
+        /**
+         * css associated with the import
+         */
+        css?: string;
+        /**
+         * define if the module is external to the css and add it to the import
+         */
+        external?: boolean;
+        /**
+         * cache the postcss process
+         */
+        cache?: boolean;
+    }
+
+    export function resolve(
+        id: string,
+        importer: string
+    ): Promise<ResolveResult>;
+
+    export function pluginImport(options?: Options): Plugin;
 }
